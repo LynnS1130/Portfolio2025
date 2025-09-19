9fbee4cdb752a325614294c67cc94908439404d8
LynnS1130 <latoya.stewart1130@gmail.com>
2025-09-19 14:35:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// 🎮 Latoya's first JavaScript
+let xp = 0;
+
+function gainXP(amount) {
+  xp += amount;
+  console.log("XP:", xp);
+  document.getElementById("status").textContent = `Status: XP ${xp}`;
+}
 
